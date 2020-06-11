0de72fa280b2d9d5996f6a53169f80204911828e
AmericanMale <66764448+AmericanMale@users.noreply.github.com>
2020-06-11 00:10:01 -0400

    Update Password_Generator.py
    Just got bored and thought I'd save you a couple of bytes, and to shorten things up a little bit! Great work! :)

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,21 +1,19 @@
-import random
-import string
+from random import sample
+from string import printable
 
-printable_characters = string.printable[:95]*10
-right = 0
+printable_characters = printable[:95]*10
 
-while right != 1:
+while True:
     try:
         user_input = int(input("Choose a lenght for you password "
                                "(min. 6 and max. 50 characters): "))
         if user_input < 6 or user_input > 50:
-            print("Out of range!")
-            continue
-        right += 1        
-    except:
+            raise ValueError
+            continue        
+    except ValueError:
         print("Wrong input, only numbers between 6 and 50 are allowed.")
         
-password = (random.sample(printable_characters, user_input))
-final_password = random.sample(password, user_input)
+password = (sample(printable_characters, user_input))
+final_password = sample(password, user_input)
 final_password_as_string = " ".join(final_password).replace(" ", "")
 print("Generated password for you: " + final_password_as_string)
